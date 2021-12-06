833be7a1421111f6ff614a82f33624ccd9a12738
jeff-copeman-fioneer <95645864+jeff-copeman-fioneer@users.noreply.github.com>
2021-12-06 14:37:37 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "Fioneer"
 
     workspaces {
       name = "sentinel-example"
